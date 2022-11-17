5c38a3835195ba13d4f9102171411226e6db390f
Yad <60503097+Yadzzz@users.noreply.github.com>
2022-11-17 17:16:21 +0100

    323442

--- TuraProductsViewer/Pages/PdfCreator.cs
+++ TuraProductsViewer/Pages/PdfCreator.cs
@@ -86,16 +86,42 @@ namespace TuraProductsViewer.Pages
             //set converter options
             converter.Options.PdfPageSize = PdfPageSize.A4;
             converter.Options.PdfPageOrientation = PdfPageOrientation.Portrait;
-            //converter.Options.WebPageWidth = 1024;
-            //converter.Options.WebPageHeight = 0;
-
             converter.Options.PdfStandard = PdfStandard.PdfA;
 
             //converter.Options.MarginLeft = 0;
             //converter.Options.MarginRight = 0;
-            converter.Options.AutoFitHeight = HtmlToPdfPageFitMode.AutoFit;
-            converter.Options.AutoFitWidth = HtmlToPdfPageFitMode.AutoFit;
-            converter.Options.WebPageWidth = 992;
+            //converter.Options.AutoFitHeight = HtmlToPdfPageFitMode.AutoFit;
+            //converter.Options.AutoFitWidth = HtmlToPdfPageFitMode.AutoFit;
+            //converter.Options.WebPageWidth = 992;
+
+
+            //Header settings
+            converter.Options.DisplayHeader = true;
+            converter.Header.DisplayOnFirstPage = true;
+            converter.Header.DisplayOnOddPages = true;
+            converter.Header.DisplayOnEvenPages = true;
+            converter.Header.Height = 60;
+
+            PdfHtmlSection headerHtml = new PdfHtmlSection("<style>.header { padding-top:0px; width: 100%; height:150px; text-align: center; background: #3F3F3F;  color: white;  font-size: 40px;}</style><div class=\"header\"><h3>Tura Scaninavia</h3></div>", "");
+            headerHtml.AutoFitHeight = HtmlToPdfPageFitMode.AutoFit;
+            converter.Header.Add(headerHtml);
+
+            //Footer settings
+            converter.Options.DisplayFooter = true;
+            converter.Footer.DisplayOnFirstPage = true;
+            converter.Footer.DisplayOnOddPages = true;
+            converter.Footer.DisplayOnEvenPages = true;
+            converter.Footer.Height = 40;
+
+            // page numbers can be added using a PdfTextSection object
+            //PdfTextSection text = new PdfTextSection(0, 8, "Page: {page_number} of {total_pages}  ", new System.Drawing.Font("Arial", 8));
+            //text.HorizontalAlign = PdfTextHorizontalAlign.Right;
+            //converter.Footer.Add(text);
+
+            PdfTextSection text2 = new PdfTextSection(0, 0, "Tura Scandinavia AB - Tura +46 (0)300 56 89 20  info@turascandinavia.com", new System.Drawing.Font("Arial", 8));
+            text2.HorizontalAlign = PdfTextHorizontalAlign.Center;
+            converter.Footer.Add(text2);
+
 
             // create a new pdf document converting an url
             PdfDocument doc = converter.ConvertHtmlString(htmlString);
